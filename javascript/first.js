d8a3aee6fd40f7baebeeb4d5f01b163ff204b1e1
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-03-03 16:02:33 +0100

    update esercizio completato e rivisto

--- javascript/first.js
+++ javascript/first.js
@@ -1,7 +1,6 @@
 var kmPercorsi = prompt("Quanti km devi percorrere?");
 var eta = prompt("Quanti anni hai?");
 var costo = kmPercorsi * 0.21;
-var costoDef = costo.toFixed(2);
 
 console.log(costo + "€");
 
@@ -9,14 +8,16 @@ var minorenni = eta < 18;
 var old = eta > 65;
 
 
-var costoMinorenni = (costoDef * 80) / 100;
-var costoOld = (costoDef * 60) / 100;
+var costoMinorenni = (costo * 80) / 100;
+var costoMinorenniFix = costoMinorenni.toFixed(2);
+var costoOld = (costo * 60) / 100;
+var costoOldFix = costoOld.toFixed(2);
 
 
 if (minorenni) {
   console.log(costoMinorenni);
-  document.getElementById('biglietto').innerHTML = "Il biglietto costa: " + costoMinorenni + "€";
+  document.getElementById('biglietto').innerHTML = "Il biglietto costa: " + costoMinorenniFix + "€";
 }else if (old) {
   console.log(costoOld);
-  document.getElementById('biglietto').innerHTML = "Il biglietto costa: " + costoOld + "€";
+  document.getElementById('biglietto').innerHTML = "Il biglietto costa: " + costoOldFix + "€";
 }
