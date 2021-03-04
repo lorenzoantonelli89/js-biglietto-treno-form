c2b8e55814c5c61da38d9d1181ffc684f971ed5a
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-03-04 09:50:39 +0100

    update forma + contratta con - var

--- javascript/first.js
+++ javascript/first.js
@@ -1,26 +1,41 @@
+// var kmPercorsi = prompt("Quanti km devi percorrere?");
+// var eta = prompt("Quanti anni hai?");
+// var costo = kmPercorsi * 0.21;
+//
+// console.log(costo + "€");
+//
+// var minorenni = eta < 18;
+// var old = eta > 65;
+//
+//
+// var costoMinorenni = (costo * 80) / 100;
+// var costoMinorenniFix = costoMinorenni.toFixed(2);
+// var costoOld = (costo * 60) / 100;
+// var costoOldFix = costoOld.toFixed(2);
+//
+//
+// if (minorenni) {
+//   console.log(costoMinorenni);
+//   document.getElementById('biglietto').innerHTML = "Il biglietto costa: " + costoMinorenniFix + "€";
+// }else if (old) {
+//   console.log(costoOld);
+//   document.getElementById('biglietto').innerHTML = "Il biglietto costa: " + costoOldFix + "€";
+// }else {
+//   console.log(costo);
+//   document.getElementById('biglietto').innerHTML = "Il biglietto costa: " + costo + "€";
+// }
+
+// alternativa con meno javascript
+
 var kmPercorsi = prompt("Quanti km devi percorrere?");
 var eta = prompt("Quanti anni hai?");
 var costo = kmPercorsi * 0.21;
 
-console.log(costo + "€");
-
-var minorenni = eta < 18;
-var old = eta > 65;
-
-
-var costoMinorenni = (costo * 80) / 100;
-var costoMinorenniFix = costoMinorenni.toFixed(2);
-var costoOld = (costo * 60) / 100;
-var costoOldFix = costoOld.toFixed(2);
-
-
-if (minorenni) {
-  console.log(costoMinorenni);
-  document.getElementById('biglietto').innerHTML = "Il biglietto costa: " + costoMinorenniFix + "€";
-}else if (old) {
-  console.log(costoOld);
-  document.getElementById('biglietto').innerHTML = "Il biglietto costa: " + costoOldFix + "€";
-}else {
-  console.log(costo);
-  document.getElementById('biglietto').innerHTML = "Il biglietto costa: " + costo + "€";
+if (eta < 18) {
+  costo = (costo / 100 * 80).toFixed(2);
+}else if (eta > 65) {
+  costo = (costo / 100 * 60).toFixed(2);
 }
+
+console.log(costo + "€");
+document.getElementById('biglietto').innerHTML = "Il biglietto costa: " + costo + "€";
